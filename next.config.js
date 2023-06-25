04ed3f8c3ec1b89eef8da52c3b98d80490bb5360
Sam Petering <samuel.petering@gmail.com>
2023-06-25 18:34:10 -0500

    initial commit! wowzers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    domains: [
+      "nix-tag-images.s3.amazonaws.com",
+      "d2eawub7utcl6.cloudfront.net",
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "nix-tag-images.s3.amazonaws.com",
+        pathname: "*",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
